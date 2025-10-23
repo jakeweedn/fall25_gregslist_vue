37efcd5aab6b8fb419ee7b3b0e1516fbc2426279
Jake <weednj@gmail.com>
2025-10-23 13:33:55 -0600

    Got houses!

--- src/pages/HousesPage.vue
+++ src/pages/HousesPage.vue
@@ -1,5 +1,8 @@
 <script setup>
 import { AppState } from '@/AppState.js';
+import { housesService } from '@/services/HousesService.js';
+import { logger } from '@/utils/Logger.js';
+import { Pop } from '@/utils/Pop.js';
 import { computed, onMounted } from 'vue';
 
 const houses = computed(() => AppState.houses)
@@ -12,6 +15,19 @@ onMounted(() => {
 })
 
 async function getHouses() {
+    try {
+        await housesService.getHouses()
+
+
+    }
+
+    catch (error) {
+
+        Pop.error(error)
+        logger.error('COULD NOT GET HOUSES', error)
+
+
+    }
 
 
 }
--- src/services/HousesService.js
+++ src/services/HousesService.js
@@ -1,6 +1,15 @@
+import { logger } from "@/utils/Logger.js"
+import { api } from "./AxiosService.js"
 
 
 class HousesService {
+
+    async getHouses() {
+
+        const response = await api.get('api/houses')
+        logger.log('GOT HOUSES', response.data)
+
+    }
 
 
 }
